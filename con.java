7b7bd31f75b4ff8851ede5fd99f9bbcd2c305210
Hirun dilshan <72131409+Hirun123@users.noreply.github.com>
2023-02-26 22:29:53 +0530

    Add files via upload

---
+++ con.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package librarymanagementsystem;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author HP
+ */
+class con {
+
+    static PreparedStatement PrepareStatement(String select_membernamebook1returndateDATEDIFFn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
